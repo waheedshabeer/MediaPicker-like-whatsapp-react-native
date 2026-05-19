ae79144b18160733ea02f286c61bdc0e905d5a2a
Waheed Shabeer <waheed.shabeer@gmail.com>
2026-05-20 00:59:10 +0500

    Initial setup for React Native project with Babel, Metro bundler, basic index file and ESLint configuration.

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
-/**
- * @format
- */
+import React from 'react';
+import { View, Text } from 'react-native';
 
-import {AppRegistry} from 'react-native';
-import App from './App';
-import {name as appName} from './app.json';
+function App() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Hello World!</Text>
+    </View>
+  );
+}
 
-AppRegistry.registerComponent(appName, () => App);
+export default App;
