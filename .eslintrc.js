a69990bc43f95647ab19726dda8ce41ac728894f
Waheed Shabeer <waheed.shabeer@gmail.com>
2026-05-21 23:01:34 +0500

    Refactor .eslintrc.js and .prettierrc.js for better linting and formatting

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,1 +1,10 @@
-{}
+{
+  "extends": [
+    "airbnb",
+    "plugin:react/recommended"
+  ],
+  "rules": {
+    "no-console": [2, { "allow": ["warn", "error"] }],
+    "import/no-default-export": [0]
+  }
+}
