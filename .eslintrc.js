45e7a3da79d73ebd709620faee9b289293be8f49
Waheed Shabeer <waheed.shabeer@gmail.com>
2026-05-22 15:01:43 +0500

    Initial setup of project files

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,9 +1,3 @@
-// Add your ESLint configuration rules here
-// Example:
-/* eslint-disable camelcase */
 module.exports = {
-  extends: ['react-native'],
-  globals: {
-    React: true,
-  },
+  extends: 'react-native'
 };
